52803f2cbf99a45aefea43618add693cbc100efe
Dominykas Blyžė <hello@dominykas.com>
2013-10-01 14:17:25 +0100

    done?

---
+++ tasks/checkpending.js
@@ -0,0 +1,33 @@
+/*
+ * grunt-checkpending
+ * https://github.com/dymonaz/grunt-checkpending
+ *
+ * Copyright (c) 2013 Dominykas Blyžė
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var shell = require("shelljs");
+
+module.exports = function (grunt) {
+
+	grunt.registerTask('checkpending', 'Check that we have no pending changes before proceeding.', function (force) {
+
+		if (grunt.option('no-checkpending') && !force) {
+			grunt.log.writeln("Pending check overridden via command line.");
+			return;
+		}
+
+		var statusOutput = shell.exec("git status -s");
+		if (statusOutput.code !== 0) {
+			grunt.fail.fatal("Failed to detect the current status");
+		}
+
+		var status = statusOutput.output.trim();
+		if (status !== "") {
+			grunt.fail.fatal("You have pending changes.");
+		}
+
+	});
+};
---
+++ test/checkpending.test.js
@@ -0,0 +1,81 @@
+var buster = require('buster');
+var expect = buster.referee.expect;
+var shell = require('shelljs');
+shell.config.silent = true;
+
+var GRUNT_SUCCESS = "Done, without errors.";
+var GRUNT_FATAL = "Fatal error:";
+
+var execGrunt = function (cmd) {
+	shell.pushd('tmp');
+	var output = shell.exec("grunt " + cmd);
+	shell.popd();
+	return output;
+};
+
+buster.testCase("grunt-checkpending", {
+
+	"setUp": function () {
+		shell.mkdir("-p", "tmp");
+		shell.pushd('tmp');
+		shell.exec("git init");
+		shell.cp("../test/fixture-gruntfile.js", "Gruntfile.js");
+		shell.exec("git add -A");
+		shell.exec("git commit -m 'initial commit'");
+		shell.popd();
+	},
+
+	"tearDown": function () {
+		shell.rm("-rf", "tmp");
+	},
+
+	"should not proceed when there's unstaged changes": function () {
+		shell.pushd('tmp');
+		shell.exec("touch test.file");
+		shell.popd();
+
+		var output = execGrunt("checkpending");
+		expect(output.output).toMatch(GRUNT_FATAL);
+		expect(output.output).toMatch("you have pending changes");
+		expect(output.code).toEqual(1, "Incorrect grunt output code");
+	},
+
+	"should proceed when there are no changes": function () {
+		var output = execGrunt("checkpending");
+		expect(output.output).toMatch(GRUNT_SUCCESS);
+		expect(output.code).toEqual(0, "Incorrect grunt output code");
+	},
+
+	"should bypass via command line": function () {
+		shell.pushd('tmp');
+		shell.exec("touch test.file");
+		shell.popd();
+
+		var output = execGrunt("checkpending --no-checkpending");
+		expect(output.output).toMatch(GRUNT_SUCCESS);
+		expect(output.code).toEqual(0, "Incorrect grunt output code");
+	},
+
+	"should not bypass via command line but forced via options": function () {
+		shell.pushd('tmp');
+		shell.exec("touch test.file");
+		shell.popd();
+
+		var output = execGrunt("checkpending:true --no-checkpending");
+		expect(output.output).toMatch(GRUNT_FATAL);
+		expect(output.output).toMatch("you have pending changes");
+		expect(output.code).toEqual(1, "Incorrect grunt output code");
+	},
+
+	"should fail when git fails": function () {
+		shell.pushd('tmp');
+		shell.exec("echo 'corrupt' >.git/index"); // corrupt git index to trigger an error
+		shell.popd();
+
+		var output = execGrunt("checkpending");
+		expect(output.output).toMatch(GRUNT_FATAL);
+		expect(output.output).toMatch("Failed to detect");
+		expect(output.code).toEqual(1, "Incorrect grunt output code");
+	}
+
+});
